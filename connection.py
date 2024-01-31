62930cf1638dfa61eea2a556fa5af1ef116e8951
mohammad-hoseini <mohammadrezajalahoseini@yahoo.com>
2024-01-31 18:23:28 +0330

    user register done

--- connection.py
+++ connection.py
@@ -1,30 +1,12 @@
 import mysql.connector
 
-db_config = {
-    "host": "cho-oyu.liara.cloud",
-    "port": 32657,
-    "user": "root",
-    "password": "KILJPqrK7wpJEssWqbO9mv4g",
-    "database": "unruffled_lalande",
-}
-
-
-class ConnectionManager:
-    def __init__(self, config=db_config):
-        self.config = config
-        self.db = None
-
-    def __enter__(self):
-        try:
-            print("db config")
-
-            # Connect to the MySQL database
-            self.db = mysql.connector.connect(**(self.config))
-
-            return self.db.cursor()
-
-        except mysql.connector.Error as err:  # connection error
-            raise Exception("Connection Error")
-
-    def __exit__(self, type, value, traceback):
-        self.db.close()
+try:
+    connection = mysql.connector.connect(
+        host="cho-oyu.liara.cloud",
+        port=32657,
+        user="root",
+        password="KILJPqrK7wpJEssWqbO9mv4g",
+        database="unruffled_lalande",
+    )
+except mysql.connector.Error as err:
+    raise Exception("Connection Error")
