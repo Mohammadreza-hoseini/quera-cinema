068597e6aca8bfffc6dbbc38c6149f7e01ccd960
mohammad-hoseini <mohammadrezajalahoseini@yahoo.com>
2024-02-03 12:49:40 +0330

    add decorator.py for login user and add change username and change password

--- users.py
+++ users.py
@@ -1,6 +1,7 @@
 import re
 import hashlib
 from connection import connection
+from decorator import login_decorator
 
 cursor = connection.cursor()
 
@@ -119,10 +120,34 @@ class Users:
         else:
             print('username or password is wrong')
 
+    @staticmethod
+    @login_decorator
+    def change_user_name(user_name):
+        new_user_name = input('Enter new username: ')
+        cursor.execute(
+            f"UPDATE User SET username='{new_user_name}' where username='{user_name}'"
+        )
+        connection.commit()
+
+    @staticmethod
+    @login_decorator
+    def change_password(user_name):
+        new_password = input('Enter new password: ')
+        confirm_password = input('Enter password again: ')
+        if new_password != confirm_password:
+            print('password is not match')
+            return False
+        cursor.execute(
+            f"UPDATE User SET password='{hashlib.sha256(new_password.encode()).hexdigest()}' where username='{user_name}'"
+        )
+        connection.commit()
+        print('password change')
+
 
 user_manager = Users()
 
-chose_login_or_register = int(input("For register enter 1 and for login enter 2: "))
+chose_login_or_register = int(
+    input("For register enter 1: \nFor login enter 2: \nFor change username enter 3: \nFor change password enter 4:"))
 if chose_login_or_register == 1:
     username = input("Enter your username: ")
     email = input("Enter your email: ")
@@ -134,3 +159,7 @@ elif chose_login_or_register == 2:
     username = input("Enter your username: ")
     password = input("Enter your password: ")
     user_manager.login(username, password)
+elif chose_login_or_register == 3:
+    user_manager.change_user_name()
+elif chose_login_or_register == 4:
+    user_manager.change_password()
