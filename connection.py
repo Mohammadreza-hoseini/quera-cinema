fe538562889f56b8ff3bb294637d698516fc947a
arman <shakerian.arman@yahoo.com>
2024-02-03 12:33:35 +0330

    add connection

---
+++ connection.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+try:
+    connection = mysql.connector.connect(
+        host="cho-oyu.liara.cloud",
+        port=32657,
+        user="root",
+        password="KILJPqrK7wpJEssWqbO9mv4g",
+        database="unruffled_lalande",
+    )
+except mysql.connector.Error as err:
+    raise Exception("Connection Error")
+# finally:
+#     connection.close()
