84291199a7de5837fdf625029fb26c61f8202dbc
mohammad-hoseini <mohammadrezajalahoseini@yahoo.com>
2024-02-04 22:03:12 +0330

    commit after refactor users.py and add new method for admin and add custom exception

---
+++ errors.py
@@ -0,0 +1,57 @@
+class CustomError(Exception):
+    def __init__(self, error_message):
+        Exception.__init__(self)
+        self.error_message = error_message
+
+    def __str__(self):
+        return self.error_message
+
+
+class UnknownError(CustomError):
+    def __init__(self):
+        CustomError.__init__(self, 'Internal server error')
+
+
+class InvalidArguments(CustomError):
+    def __init__(self):
+        CustomError.__init__(self, 'Invalid arguments')
+
+
+class InvalidUsernameOrPassword(CustomError):
+    def __init__(self):
+        CustomError.__init__(self, 'Username or Password is wrong')
+
+
+class InvalidEmail(CustomError):
+    def __init__(self):
+        CustomError.__init__(self, 'Email is invalid')
+
+
+class InvalidPassword(CustomError):
+    def __init__(self):
+        CustomError.__init__(self, 'Password is invalid')
+
+
+class InvalidUsername(CustomError):
+    def __init__(self):
+        CustomError.__init__(self, 'Username is invalid')
+
+
+class ChangePassword(CustomError):
+    def __init__(self):
+        CustomError.__init__(self, 'Password not match')
+
+
+class UsernameExist(CustomError):
+    def __init__(self):
+        CustomError.__init__(self, 'Username exist')
+
+
+class EmailExist(CustomError):
+    def __init__(self):
+        CustomError.__init__(self, 'Email exist')
+
+
+class InvalidPhoneNumber(CustomError):
+    def __init__(self):
+        CustomError.__init__(self, 'phone number is invalid')
