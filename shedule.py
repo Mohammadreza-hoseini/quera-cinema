671a50c201f495b32c5ef624b586424bdef1b30c
BehnamAslani <thebehnamaslani@gmail.com>
2024-02-05 16:34:05 -0500

    add time list to Schedule Class Introducing list of movie with date filter that filters date from now to later and displaying movie name wit theater name  and date on schedule

--- shedule.py
+++ shedule.py
@@ -8,30 +8,46 @@ class Schedule:
         pass
 
     @classmethod
-    def Add_Schedule(cls, movie_id, theater_id, on_screen_time):
+    def add_schedule(cls, movie_id, theater_id, on_screen_time):
 
         # movie existing query
-        cursor.execute(f"SELECT id FROM Movie WHERE id = {movie_id}")
+        cursor.execute(f"SELECT id FROM Movie WHERE id = '{movie_id}'")
         movie_data = cursor.fetchone()
 
+        # movie existing query
         if movie_data is not None:
-            movie_id_result = movie_data[0]
+            movie_data = movie_data[0]
         else:
             return "Movie does not exist "
 
         # theater existing query
-        cursor.execute(f"SELECT id FROM Theater WHERE id = {theater_id}")
+        cursor.execute(f"SELECT id FROM Theater WHERE id = '{theater_id}'")
         theater_data = cursor.fetchone()
 
         if theater_data is not None:
             theater_id_result = theater_data[0]
         else:
-            return "Movie does not exist"
+            return "theater does not exist"
 
         cursor.execute(
-            f'insert into Schedule(id, movie_id, theater_id, on_screen_time)values (uuid(),\
-             {movie_id_result.__repr__()}, {theater_id_result.__repr__()}, {on_screen_time.__repr__()})')
+            f'insert into Schedule(id, movie_id, theater_id, on_screen_time)values (uuid(),"{movie_data}", "{theater_id_result}", "{on_screen_time}")')
         connection.commit()
 
+    @classmethod
+    def time_list(cls):
 
-a = Schedule.Add_Schedule('10', '1', '2023-10-10 11:00:00')
+        cursor.execute(
+            f'select * from Schedule where on_screen_time >= now();'
+        )
+        time_list_date = cursor.fetchall()
+
+        for i in time_list_date:
+            cursor.execute(f'select name from Movie where id = "{i[1]}"')
+            movie_name = cursor.fetchone()[0]
+            cursor.execute(f'select name from Theater where id = "{i[2]}"')
+            theater_name = cursor.fetchone()[0]
+            print(i[0], movie_name, theater_name, i[3], sep='|---|')
+
+
+# a = Schedule.add_schedule('10', '41ddc2d2-6613-4a53-96ba-97348496d3b8', '2025-10-11 10:00:00')
+Schedule.time_list()
