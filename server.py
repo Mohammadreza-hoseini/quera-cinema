6a3ba85e734b4e682dcaf0d36e351187067e2bc6
arman <shakerian.arman@yahoo.com>
2024-02-13 22:43:22 +0330

    feat: request thread

--- server.py
+++ server.py
@@ -7,35 +7,50 @@ from bankaccount import BankAccount
 from users import Users
 from wallet import Wallet
 
+def request_thread(client_socket, addr, data):
+    if isinstance(data, tuple):
+        command, user_id = data
+    else:
+        command = data
+                
+    print("baba1", command)
+    if command == "1":  # register
+        register_func = Users.register
+        # convert to byte
+        pickled_function = pickle.dumps(register_func)
+        client_socket.send(pickled_function)
+
+    if command == "2": #login
+        login_func = Users.login
+        # convert to byte
+        pickled_function = pickle.dumps(login_func)
+        client_socket.send(pickled_function)
+    
+    if command == "3": #change username
+        change_username_func = Users.change_user_name
+        print("baba2", type(change_username_func))
+        # convert to byte
+        pickled_function = pickle.dumps(change_username_func)
+        client_socket.send(pickled_function)
+        
 def handle_client(client_socket, addr):
     try:
         while True:
             request = client_socket.recv(1024)
             data = pickle.loads(request)
             print("ssssssssssssssssss", type(data))
-            if isinstance(data, tuple):
-                command, user_id = data
-            else:
-                command = data
-            print("baba1", command)
-            if command == "1":  # register
-                register_func = Users.register
-                # convert to byte
-                pickled_function = pickle.dumps(register_func)
-                client_socket.send(pickled_function)
-
-            if command == "2": #login
-                login_func = Users.login
-                # convert to byte
-                pickled_function = pickle.dumps(login_func)
-                client_socket.send(pickled_function)
             
-            if command == "3": #change username
-                change_username_func = Users.change_user_name
-                print("baba2", type(change_username_func))
-                # convert to byte
-                pickled_function = pickle.dumps(change_username_func)
-                client_socket.send(pickled_function)
+            
+            thread = threading.Thread(
+                target=request_thread,
+                args=(
+                    client_socket,
+                    addr, data
+                ),
+            )
+            thread.start()
+            
+            
             
             
             
@@ -63,14 +78,15 @@ def run_server():
         while True:
             client_socket, addr = server.accept()
             print(f"Accepted connection from {addr[0]}:{addr[1]}")
-            thread = threading.Thread(
-                target=handle_client,
-                args=(
-                    client_socket,
-                    addr,
-                ),
-            )
-            thread.start()
+            handle_client(client_socket, addr)
+            # thread = threading.Thread(
+            #     target=handle_client,
+            #     args=(
+            #         client_socket,
+            #         addr,
+            #     ),
+            # )
+            # thread.start()
     except Exception as e:
         print(f"Error: {e}")
     finally:
