ee23e5d18bd7cc67b088349ff4d7a9e20122e891
arman <shakerian.arman@yahoo.com>
2024-02-15 13:04:56 +0330

    Debug: multiple clients

--- server.py
+++ server.py
@@ -5,57 +5,68 @@ import threading
 
 from bankaccount import BankAccount
 from users import Users
-from wallet import Wallet
+from wallet import Wallet, wallet_menu
 
 # thread_list = []
 
+
 def request_thread(client_socket, addr, data):
     print("in_server: request_thread")
-    
+
     if isinstance(data, tuple):
         command, user_id = data
     else:
         command = data
 
-                
     if command == "1":  # register
         register_func = Users.register
         # convert to byte
         pickled_function = pickle.dumps(register_func)
         client_socket.send(pickled_function)
 
-    if command == "2": #login
+    if command == "2":  # login
         login_func = Users.login
         # convert to byte
         pickled_function = pickle.dumps(login_func)
         client_socket.send(pickled_function)
-    
-    if command == "3": #change username
+
+    if command == "3":  # change username
         change_username_func = Users.change_user_name
         # convert to byte
         pickled_function = pickle.dumps(change_username_func)
-        print("hereee")
         client_socket.send(pickled_function)
-        print("in_server: function sent")
+    
+    if command == "4":  # change password
+        change_username_func = Users.change_password
+        # convert to byte
+        pickled_function = pickle.dumps(change_username_func)
+        client_socket.send(pickled_function)
+        
+        
+    if command == "9":  # wallet #TODO
+        wallet_menu_func = wallet_menu
+        # convert to byte
+        pickled_function = pickle.dumps(wallet_menu_func)
+        client_socket.send(pickled_function)
+    
     print("return request_thread")
     return
-        
+
+
 def handle_client(client_socket, addr):
     while True:
         try:
+            print("waiting for req")
             request = client_socket.recv(1024)
+            if not request:
+                break
             data = pickle.loads(request)
-            
+
             print("in_server: handle client", data)
-            
-            
-            
+
             thread = threading.Thread(
                 target=request_thread,
-                args=(
-                    client_socket,
-                    addr, data
-                ),
+                args=(client_socket, addr, data),
             )
             # thread_list.append(thread)
             print("thread start")
@@ -63,17 +74,18 @@ def handle_client(client_socket, addr):
             print("thread waiting")
             thread.join()
             print("thread finished")
-            
+
         except Exception as e:
             print(f"Error when hanlding client: {e}")
             break
         # finally:
         #     # thread.join()
         #     print("thread finished")
-            
+
         #     client_socket.close()
         #     print(f"Connection to client ({addr[0]}:{addr[1]}) closed")
         #     break
+    client_socket.close()
 
 
 def run_server():
@@ -86,17 +98,18 @@ def run_server():
         print(f"Listening on {server_ip}:{port}")
 
         while True:
+            print(1)
             client_socket, addr = server.accept()
             print(f"Accepted connection from {addr[0]}:{addr[1]}")
-            handle_client(client_socket, addr)
-            # thread = threading.Thread(
-            #     target=handle_client,
-            #     args=(
-            #         client_socket,
-            #         addr,
-            #     ),
-            # )
-            # thread.start()
+            # handle_client(client_socket, addr)
+            user_thread = threading.Thread(
+                target=handle_client,
+                args=(
+                    client_socket,
+                    addr,
+                ),
+            )
+            user_thread.start()
     except Exception as e:
         print(f"Error: {e}")
     finally:
